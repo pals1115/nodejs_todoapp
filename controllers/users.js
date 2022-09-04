45328e530a3c95af7fffa8339636b62a25926450
pals1115 <pswagh1101@gmail.com>
2022-09-04 21:08:44 +0530

    Updated APIs

--- controllers/users.js
+++ controllers/users.js
@@ -1,37 +1,40 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 let users = [];
+// return all users
 export const getUsers = (req, res) => {
-    res.send(users);
-}
+  res.send(users);
+};
 
 export const createUser = (req, res) => {
+  const user = req.body;
 
-    const users = req.body;
-
-    users.push({ ...user, id:uuidv4() });
-    res.send(`User with the name ${user.firstName} added to the database!`);
-}
+  users.push({ ...user, id: uuidv4() });
+  res.send(`User with the name ${users[0].id} added to the database!`);
+};
 
 export const getUser = (req, res) => {
-    const {id} = req.params;
-    const foundUser = users.find((user) => user.id === id);
-    res.send(foundUser);
-}
+  const { id } = req.params;
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
+};
 
 export const deleteUser = (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    users = users.filter((user) => user.id !== id);
-    res.send(`User with the id ${id} deleted from the database.`);
-}
-
+  users = users.filter((user) => user.id !== id);
+  res.send(`User with the id ${id} deleted from the database.${users.length} `);
+};
 export const updateUser = (req, res) => {
-    const { id } = req.params;
-    const {firstName, lastName, age } = req.body;
-    const user = users.find((user) => user.id === id);
+  const { id } = req.params;
+  const user = users.find((user) => user.id === id);
+  if (user) {
+    const { firstName, lastName, age } = req.body;
 
-    if(firstName) user.firstName = firstName;
-    if(lastName) user.lastName = lastName;
-    if(age) user.age = age;
+    if (firstName) user.firstName = firstName;
+    if (lastName) user.lastName = lastName;
+    if (age) user.age = age;
     res.send(`User with the id ${id} has been updated`);
-}
+  } else {
+    res.send(`User with the id ${id} not found`);
+  }
+};
